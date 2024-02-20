ea7760acb8795f333b459887f96fd1b0b6085b09
Prem Sagar Gupta <premsagarg23@gmail.com>
2024-02-20 15:54:44 +0545

    CloneCompleted

---
+++ script.js
@@ -0,0 +1,145 @@
+let videocon = document.querySelector("#video-container");
+let play = document.querySelector("#play");
+
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
+locomotiveAnimation();
+
+
+function navAnimation(){
+    gsap.to(".navp1 svg", {
+        transform: "translateY(-100%)",
+        scrollTrigger: {
+          trigger: "#page1",
+          scroller: "#main",
+          start: "top 0",
+          end: "top -5%",
+          scrub: true,
+        },
+      });
+      
+      gsap.to(".navp2 .links", {
+        transform: "translateY(-100%)",
+        opacity: 0,
+        scrollTrigger: {
+          trigger: "#page1",
+          scroller: "#main",
+          start: "top 0",
+          end: "top -5%",
+          scrub: true,
+        },
+      });
+}
+
+navAnimation();
+
+
+function videoAnimation() {
+  videocon.addEventListener("mouseenter", function () {
+    // play.style.opacity = 1;
+    // play.style.scale = 1;
+    gsap.to(play, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(play, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+
+  videocon.addEventListener("mousemove", function (e) {
+    gsap.to(play, {
+      left: e.x - 80,
+      top: e.y - 80,
+    });
+  });
+}
+
+videoAnimation();
+
+
+function loadingAnimation() {
+  gsap.from("#page1 h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.3,
+  });
+  gsap.from("#page1 #video-container", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.5,
+  });
+}
+
+loadingAnimation();
+
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (e) {
+    gsap.to("#cursor", {
+      left: e.x,
+      top: e.y,
+    });
+  });
+
+  document.querySelectorAll(".child").forEach(function (prop) {
+    prop.addEventListener("mouseenter", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%, -50%) scale(1)",
+      });
+    });
+
+    prop.addEventListener("mouseleave", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%, -50%) scale(0)",
+      });
+    });
+  });
+}
+
+cursorAnimation();
